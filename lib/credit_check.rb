62b8ec16ae53f25cd84893113645ea2828f9d70e
Devin Pile <devinpile@gmail.com>
2023-11-28 10:29:37 -0700

    Add code for luhn algorithm method and examples for tests

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -27,8 +27,34 @@ card_number = "5541808923795240"
     #check to see if the sum from the last step is a multiple of ten using modulo operator (%)
     #if so, return the valid output string - if not, return the invalid output string
 
-
+def is_valid?(cc_num)
+    card_number_arr = cc_num.to_i.digits 
+    card_number_arr.each_with_index do |num,index|
+        card_number_arr[index] = num * 2 if index.odd? 
+    end
+    card_number_arr.map! { |num| num > 9 ? num - 9 : num }
+    card_number_arr.sum % 10 == 0
+end
 
 # Output
 ## If it is valid, print "The number [card number] is valid!"
 ## If it is invalid, print "The number [card number] is invalid!"
+
+p is_valid?(card_number)
+
+#Valid:
+test1 = 5541808923795240
+test2 = 4024007136512380
+test3 = 6011797668867828
+
+#Invalid: 
+test4 = 5541801923795240 
+test5 = 4024007106512380
+test6 = 6011797668868728
+
+p is_valid?(test1)
+p is_valid?(test2)
+p is_valid?(test3)
+p is_valid?(test4)
+p is_valid?(test5)
+p is_valid?(test6)
